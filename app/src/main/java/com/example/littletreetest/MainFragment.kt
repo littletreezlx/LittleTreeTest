99e837b7159d4c4476650effe6c7e760a29cb433
LittleTree <602788458@qq.com>
2020-06-07 22:15:02 +0800

    test NodeTreeAdapter

--- app/src/main/java/com/example/littletreetest/MainFragment.kt
+++ app/src/main/java/com/example/littletreetest/MainFragment.kt
@@ -9,6 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chad.library.adapter.base.entity.node.BaseNode
+import com.example.littletreetest.node.FirstNode
+import com.example.littletreetest.node.NodeTreeAdapter
+import com.example.littletreetest.node.SecondNode
+import com.example.littletreetest.node.ThirdNode
 import kotlinx.android.synthetic.main.main_fragment.*
 import kotlin.random.Random
 
@@ -18,7 +24,11 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-//    private lateinit var viewModel: MainViewModel
+    private val treeAdapter: NodeTreeAdapter =
+        NodeTreeAdapter()
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,19 +47,38 @@ class MainFragment : Fragment() {
             val random = Random.nextInt(100)
             vm.updateStr(random.toString())
         }
-
         vm._AllStr.observe(viewLifecycleOwner, Observer {
             tv_all_str.text = it
         })
-
         vm.subStr.observe(viewLifecycleOwner, Observer {
             tv_sub_str.text = it
         })
 
+
+
+        rv_list.run {
+            setLayoutManager(LinearLayoutManager(context))
+            setAdapter(treeAdapter)
+
+        }
+        treeAdapter.setList(generateData())
     }
 
-    private fun generateData(): ArrayList<String> {
-        val res = arrayListOf("1", "2", "3")
+
+    private fun generateData(): MutableList<BaseNode> {
+        val third = ThirdNode("third")
+        val second = SecondNode(listOf(third), "second")
+        val first = FirstNode(listOf(second), "first")
+        first.isExpanded = true
+        second.isExpanded = true
+
+        val third2 = ThirdNode("third")
+        val third1 = ThirdNode("third")
+        val second2 = SecondNode(listOf(third1, third2), "second")
+        val first2 = FirstNode(listOf(second2), "first")
+        val res = mutableListOf<BaseNode>(first, first2)
+        first2.isExpanded = true
+        second2.isExpanded = true
         return res
     }
 }
