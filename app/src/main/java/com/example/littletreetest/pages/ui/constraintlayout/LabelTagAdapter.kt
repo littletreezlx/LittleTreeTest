36a0aed2d5d46fbff36c8af7c323486ace7d500f
LittleTree <602788458@qq.com>
2021-03-04 16:46:49 +0800

    fix bugs

--- app/src/main/java/com/example/littletreetest/pages/ui/constraintlayout/LabelTagAdapter.kt
+++ app/src/main/java/com/example/littletreetest/pages/ui/constraintlayout/LabelTagAdapter.kt
@@ -34,4 +34,28 @@ class LabelTagAdapter(list: MutableList<String>) : TagAdapter<String>(list) {
         tv.setTextColor(Color.parseColor("#707070"))
     }
 
+
+//    override fun getView(parent: FlowLayout, position: Int, t: String?): View {
+//        val rootView = LayoutInflater.from(parent.context).inflate(
+//            R.layout.item_gm_create_item_label,
+//            parent, true
+//        )
+//        val tv = rootView.findViewById<TextView>(R.id.tv_label)
+//        tv.text = t
+//        return tv
+//    }
+//
+//    override fun onSelected(position: Int, view: View) {
+//        super.onSelected(position, view)
+//        val tv = view.findViewById<TextView>(R.id.tv_label)
+//        tv.setBackgroundResource(R.drawable.background_3d4456_r10)
+//        tv.setTextColor(Color.parseColor("#FFFFFF"))
+//    }
+//
+//    override fun unSelected(position: Int, view: View) {
+//        super.unSelected(position, view)
+//        val tv = view.findViewById<TextView>(R.id.tv_label)
+//        tv.setBackgroundResource(R.drawable.background_f3f4f5_r10)
+//        tv.setTextColor(Color.parseColor("#707070"))
+//    }
 }
